4594b1424a2d6aef24cb94f7437ee70338b3e730
Xenia <andreeva19-91@mail.ru>
2024-08-18 18:37:20 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-const requestInput = document.querySelector('#input-id');
-const value = requestInput.value;
 
+async function getValue () {
+    const value = await document.querySelector('#input-id').value;
+    console.log(value);
+}
 
+getValue();
+
+// обернуть обращение к value внутрь обработчика события change, и туда же нужен дебаунс
